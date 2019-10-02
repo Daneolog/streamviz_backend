0a10ae1da2eb6a07d8dc16eb77a0790da0c25fcc
Enoch Kumala <daneolog@gmail.com>
2019-10-02 13:21:00 -0400

    updated config

---
+++ config_remote.py
@@ -0,0 +1,8 @@
+class Config(object):
+    """Base config, uses staging database server."""
+    DEBUG = False
+    TESTING = False
+    MYSQL_DATABASE_USER = 'uownorg_streamvizuser'
+    MYSQL_DATABASE_PASSWORD = '1q7pdzTk6}@4'
+    MYSQL_DATABASE_DB = 'uownorg_streamviz'
+    MYSQL_DATABASE_HOST = 'localhost'
--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,10 @@
 from flask import Flask
 from flaskext.mysql import MySQL
 from flask_cors import CORS
+from config import *
 
 app = Flask(__name__)
+app.config.from_object(Config())
 CORS(app)
 
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
-app.config['MYSQL_DATABASE_DB'] = 'streamviz'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql = MySQL(app)
