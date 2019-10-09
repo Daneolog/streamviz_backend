51421afdea983b9d820f687881a8f3e9ef53037d
Enoch Kumala <daneolog@gmail.com>
2019-10-09 13:00:24 -0400

    ayy

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ class Config(object):
     """Base config, uses staging database server."""
     DEBUG = False
     TESTING = False
-    MYSQL_DATABASE_USER = 'uownorg_streamvizuser'
-    MYSQL_DATABASE_PASSWORD = '1q7pdzTk6}@4'
-    MYSQL_DATABASE_DB = 'uownorg_streamviz'
+    MYSQL_DATABASE_USER = 'root'
+    MYSQL_DATABASE_PASSWORD = 'password'
+    MYSQL_DATABASE_DB = 'streamviz'
     MYSQL_DATABASE_HOST = 'localhost'
