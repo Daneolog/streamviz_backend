86d0dd1a226a1fb0a7585d9c6cfb6800d4a325ec
Enoch Kumala <daneolog@gmail.com>
2019-11-24 15:32:02 -0500

    updated clean script

--- src/clean.py
+++ src/clean.py
@@ -11,12 +11,14 @@ from sqlalchemy import create_engine
 def clean(filename):
     df = pd.read_csv(filename)
     df['date'] = pd.to_datetime(df[['year', 'month', 'day']], errors='coerce')
+    df['e.coli.cfu'][df['e.coli.cfu'] == 'TNTC'] = np.nan
+    df = df.astype({'e.coli.cfu': 'float64'})
 
     # rename columns
     df.rename(columns={'visual_score': 'Visual Score', 'biological_score': 'Biological Score', 'conductivity.uscm': 'Conductivity (uS/cm)', 'turbidity.ntu': 'Turbidity (NTU)', 'po4.mgL': 'Phosphate (mg/l)',
                     'no3.mgL': 'Nitrate (mg/l)', 'temperature.c': 'Temperature (°C)', 'e.coli.cfu': 'E.coli (cfu)', 'ecoli.method.known': 'E.coli Method'}, inplace=True)
 
-    df.drop(['WS', 'ID', 'month', 'year', 'day', 'quarter'], axis=1, inplace=True)
+    df.drop(['WSID', 'month', 'year', 'day', 'quarter'], axis=1, inplace=True)
 
     return df
 
